3535c2b41260d7a3147fcde40c673c651551892b
Michael Vierling <mvierling@gmail.com>
2011-08-29 15:13:09 -0700

    LessonBoxHello

---
+++ LessonBoxHello/HelloWorld.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module HelloWorld
+
+open TradeLink.API
+open TradeLink.Common
+open System.IO
+
+type HelloWorld() =
+    inherit TradeLink.Common.ResponseTemplate()
+        override this.GotTick(tick : Tick) = (
+            this.senddebug("Hello F# World")
+        )
+
